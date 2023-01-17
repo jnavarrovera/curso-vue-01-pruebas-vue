73861e0802c31e2d7a3e7c043b39fb4dabf6b560
jnavarrovera <jaime.navarro@gmail.com>
2023-01-17 19:23:22 +0100

    Intentando interceptores

--- src/api/fakeShopApi.ts
+++ src/api/fakeShopApi.ts
@@ -1,7 +1,19 @@
 import axios from "axios";
 
 const fakeShopApi = axios.create({
-    baseURL:'https://api.escuelajs.co/api/v1'
+  baseURL: "https://api.escuelajs.co/api/v1",
+  // params: {
+  //     key: 'lasdkfjlsdkjfgñslkhjgvlfdskhgñ349u039dfv8fhrh0943'
+  // },
+  // headers: {
+  //     'x-token': 'kodsjflksdjfklsdjfklñsdj'
+  // }
 });
+
+// fakeShopApi.interceptors.request.use((config) => {
+//   config.headers = {...config.headers} as AxiosHeaders;
+//   config.headers.set('Authorization', 'tokeny')
+//   return config;
+// });
 
 export default fakeShopApi;
